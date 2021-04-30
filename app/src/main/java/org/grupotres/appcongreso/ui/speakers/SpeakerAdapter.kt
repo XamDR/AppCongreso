7590b1bf815d3f50c19d8664ffabff1fc791c55e
Diomes <73634652@continental.edu.pe>
2021-04-29 19:00:58 -0500

    se agrego las funciones necesarias para que funcione el background

--- app/src/main/java/org/grupotres/appcongreso/ui/speakers/SpeakerAdapter.kt
+++ app/src/main/java/org/grupotres/appcongreso/ui/speakers/SpeakerAdapter.kt
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.grupotres.appcongreso.core.Speaker
+import org.grupotres.appcongreso.databinding.ItemLectureDetailBinding
 import org.grupotres.appcongreso.databinding.ItemSpeakerDetailBinding
+import org.grupotres.appcongreso.util.setOnClickListener
 
 class SpeakerAdapter(private val viewModel: SpeakerViewModel) :
 	ListAdapter<Speaker, SpeakerAdapter.SpeakerViewHolder>(SpeakerCallback()) {
@@ -29,8 +32,15 @@ class SpeakerAdapter(private val viewModel: SpeakerViewModel) :
 
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpeakerViewHolder {
 		val binding = ItemSpeakerDetailBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+		val holder = SpeakerViewHolder(binding)
+		holder.setOnClickListener { position, _ -> goToSpeakerDetail(position) }
 		return SpeakerViewHolder(binding)
 	}
+
+	private fun goToSpeakerDetail(position: Int) {
+		TODO("Falta implementar")
+	}
+
 
 	override fun onBindViewHolder(holder: SpeakerViewHolder, position: Int) {
 		val speaker = viewModel.speakers[position]
