6fcc07e86450a978af6674225a0fa0d58e0c88ac
Diomes <73634652@continental.edu.pe>
2021-05-06 20:11:31 -0500

    se agrego el parcelize a la clase Lecture.kt

--- app/src/main/java/org/grupotres/appcongreso/core/Lecture.kt
+++ app/src/main/java/org/grupotres/appcongreso/core/Lecture.kt
@@ -1,9 +1,12 @@
 package org.grupotres.appcongreso.core
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity
 data class Lecture(
 	@PrimaryKey val id: String,
@@ -11,4 +14,4 @@ data class Lecture(
 	@ColumnInfo(name = "end_time") val endTime: String,
 	val title: String,
 	val url: String
-)
+) : Parcelable
