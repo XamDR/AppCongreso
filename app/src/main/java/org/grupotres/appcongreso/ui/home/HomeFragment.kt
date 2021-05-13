bea6fca2fd54527b4c5ebbeb3ec679f69734cd68
SamBeCo <samuel_ibc@hotmail.com>
2021-05-12 22:52:43 -0500

    Historia 2 al 90%

--- app/src/main/java/org/grupotres/appcongreso/ui/home/HomeFragment.kt
+++ app/src/main/java/org/grupotres/appcongreso/ui/home/HomeFragment.kt
@@ -7,7 +7,9 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -131,7 +133,17 @@ class HomeFragment : Fragment() {
 		val uri = user?.photoUrl
 
 		if (uri != null) {
+
 			val imageView = (requireActivity() as MainActivity).binding.navView.findViewById<ImageView>(R.id.imageView)
+
+			auth = FirebaseAuth.getInstance()
+			val currentUser =auth.currentUser
+			//val bundle = intent.extras
+			val nameUser = (requireActivity() as MainActivity).binding.navView.findViewById<TextView>(R.id.nameUser)
+			val nameEmail = (requireActivity() as MainActivity).binding.navView.findViewById<TextView>(R.id.emailUser)
+
+			nameUser.text = currentUser?.displayName
+			nameEmail.text=currentUser?.email
 			imageView.load(uri)
 		}
 	}
