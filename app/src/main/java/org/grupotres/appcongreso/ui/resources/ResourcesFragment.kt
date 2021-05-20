c2342ab648be879db0d77afa11a93070323b5f51
SamBeCo <samuel_ibc@hotmail.com>
2021-05-20 17:50:18 -0500

    Crear lista con boton v2

--- app/src/main/java/org/grupotres/appcongreso/ui/resources/ResourcesFragment.kt
+++ app/src/main/java/org/grupotres/appcongreso/ui/resources/ResourcesFragment.kt
@@ -1,43 +1,67 @@
 package org.grupotres.appcongreso.ui.resources
 
+import android.app.DownloadManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment.DIRECTORY_DOWNLOADS
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.DividerItemDecoration
-import org.grupotres.appcongreso.data.AppRepository
-import org.grupotres.appcongreso.databinding.FragmentLectureListBinding
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 import org.grupotres.appcongreso.databinding.FragmentResourcesBinding
-import org.grupotres.appcongreso.ui.helpers.INavigator
+import java.io.File
 
 
 class ResourcesFragment : Fragment() {
 
 	private var binding: FragmentResourcesBinding? = null
-
+	lateinit var reference: StorageReference
+	lateinit var ref: StorageReference
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		binding = FragmentResourcesBinding.inflate(inflater, container, false)
+
+		binding?.download?.setOnClickListener {
+			forDownloadFiles()
+		}
 		return binding?.root
 	}
 
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-		initRecyclerView()
-	}
 
 	override fun onDestroyView() {
 		super.onDestroyView()
 		binding = null
 	}
 
-	private fun initRecyclerView() {
-		//val resourcesAdapter = ResourcesAdapter(viewModel, requireActivity() as INavigator)
-		//viewModel.lectures.observe(viewLifecycleOwner, { resourcesAdapter.submitList(it) })
-		//binding?.rvLectures?.apply {
-		//	adapter = resourcesAdapter
-		//		addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
-		//}
+	fun forDownloadFiles(){
+		val storage = Firebase.storage
+
+		reference=storage.getReference();
+		ref=reference.child("Lean.pdf");
+
+		ref.getDownloadUrl().addOnSuccessListener(OnSuccessListener<Uri> { uri ->
+			val url = uri.toString()
+			downloadFile(requireContext(), "Lean", ".pdf", DIRECTORY_DOWNLOADS, url)
+			Toast.makeText(requireContext(), "Descarga Completa", Toast.LENGTH_SHORT).show()
+		}).addOnFailureListener(OnFailureListener {
+			Toast.makeText(requireContext(), "ERROR", Toast.LENGTH_SHORT).show()
+		})
+	}
+
+	fun downloadFile(context: Context, fileName: String, fileExtension: String, destinationDirectory: String?, url: String?) {
+		val downloadmanager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+		val uri = Uri.parse(url)
+		val request = DownloadManager.Request(uri)
+		request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+		request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension)
+		downloadmanager.enqueue(request)
 	}
 }
