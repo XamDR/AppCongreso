dea88bd53279430b6eeaf59358e3139155b7b4f8
XamDR <maxdr.mat@gmail.com>
2021-05-19 22:25:09 -0500

    Se agregó conexión al calendario de Google para guardar las ponencias del congreso.

--- app/src/main/java/org/grupotres/appcongreso/ui/lectures/LectureDetailFragment.kt
+++ app/src/main/java/org/grupotres/appcongreso/ui/lectures/LectureDetailFragment.kt
@@ -1,14 +1,15 @@
 package org.grupotres.appcongreso.ui.lectures
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.provider.CalendarContract
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import coil.load
 import org.grupotres.appcongreso.R
+import org.grupotres.appcongreso.core.Lecture
 import org.grupotres.appcongreso.databinding.FragmentLectureDetailBinding
 
 class LectureDetailFragment : Fragment() {
@@ -16,9 +17,26 @@ class LectureDetailFragment : Fragment() {
 	private var binding: FragmentLectureDetailBinding? = null
 	private val args: LectureListFragmentArgs by navArgs()
 
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setHasOptionsMenu(true)
+	}
+
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		binding = FragmentLectureDetailBinding.inflate(inflater, container, false)
 		return binding?.root
+	}
+
+	override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+		inflater.inflate(R.menu.menu_fragment_lecture_detail, menu)
+	}
+
+	override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+		R.id.action_calendar -> {
+			addToCalendar(args.lectureSpeaker.lecture)
+			true
+		}
+		else -> super.onOptionsItemSelected(item)
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -39,5 +57,16 @@ class LectureDetailFragment : Fragment() {
 		binding?.speakerName?.text = args.lectureSpeaker.speakers[0].toString()
 		binding?.speakerPhoto?.load(args.lectureSpeaker.speakers[0].uriPhoto)
 		binding?.lectureDetail?.text = args.lectureSpeaker.lecture.url
+	}
+
+	private fun addToCalendar(lecture: Lecture) {
+		val intent = Intent(Intent.ACTION_INSERT).apply {
+			data = CalendarContract.Events.CONTENT_URI
+			putExtra(CalendarContract.Events.TITLE, lecture.title)
+			putExtra(CalendarContract.Events.DESCRIPTION, lecture.url)
+		}
+		if (intent.resolveActivity(requireContext().packageManager) != null) {
+			startActivity(intent)
+		}
 	}
 }
