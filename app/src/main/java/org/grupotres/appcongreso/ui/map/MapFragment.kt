b88579a3c18ec31f9c4d4783e04241563b5b279f
XamDR <maxdr.mat@gmail.com>
2021-05-20 00:19:12 -0500

    Se agregaron detalles en la inicialización del objeto GoogleMap.

--- app/src/main/java/org/grupotres/appcongreso/ui/map/MapFragment.kt
+++ app/src/main/java/org/grupotres/appcongreso/ui/map/MapFragment.kt
@@ -6,38 +6,43 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import org.grupotres.appcongreso.R
 
-class MapsFragment : Fragment() {
+class MapFragment : Fragment() {
 
 	private val callback = OnMapReadyCallback { googleMap ->
-		/**
-		 * Manipulates the map once available.
-		 * This callback is triggered when the map is ready to be used.
-		 * This is where we can add markers or lines, add listeners or move the camera.
-		 * In this case, we just add a marker near Sydney, Australia.
-		 * If Google Play services is not installed on the device, the user will be prompted to
-		 * install it inside the SupportMapFragment. This method will only be triggered once the
-		 * user has installed Google Play services and returned to the app.
-		 */
-		val sydney = LatLng(-34.0, 151.0)
-		googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-		googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+		val uc = LatLng(latitude, longitud)
+		initMapUIControls(googleMap)
+		googleMap.addMarker(MarkerOptions().position(uc).title("Marker en la Universidad Continental"))
+		googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(uc,16.0f))
 	}
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
-	): View? = inflater.inflate(R.layout.fragment_maps, container, false)
+	): View? = inflater.inflate(R.layout.fragment_map, container, false)
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
 		mapFragment?.getMapAsync(callback)
+	}
+
+	private fun initMapUIControls(googleMap: GoogleMap) {
+		googleMap.uiSettings.isMapToolbarEnabled = true
+		googleMap.uiSettings.isZoomControlsEnabled = true
+		googleMap.uiSettings.isMyLocationButtonEnabled = true
+	}
+
+	companion object {
+		// Coordenadas de la Universidad Continental
+		private const val latitude = -12.047555156621264
+		private const val longitud = -75.19878650574097
 	}
 }
