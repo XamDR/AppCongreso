951347d759dda41e7ffbd14401bfb96ff0b9ce5a
nyf <nsotacuromatos46@gmail.com>
2021-05-20 20:04:00 -0500

    correo de verificacion con JavaMail2

---
+++ app/src/main/java/org/grupotres/appcongreso/ui/lectures/Utils.java
@@ -0,0 +1,9 @@
+package org.grupotres.appcongreso.ui.lectures;
+
+public class Utils {
+    //This is your from email
+    public static final String EMAIL = "heartring13@gmail.com";
+
+    //This is your from email password
+    public static final String PASSWORD = "kanekikun";
+}
