5fee1e937af3898dd28cc34161f431fab8578682
XamDR <maxdr.mat@gmail.com>
2021-05-23 23:44:20 -0500

    Se añadieron los temas generales de cada ponencia.

--- app/src/main/java/org/grupotres/appcongreso/ui/lectures/LectureAdapter.kt
+++ app/src/main/java/org/grupotres/appcongreso/ui/lectures/LectureAdapter.kt
@@ -1,28 +1,45 @@
 package org.grupotres.appcongreso.ui.lectures
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.grupotres.appcongreso.core.LectureSpeakers
-import org.grupotres.appcongreso.databinding.ItemLectureDetailBinding
+import org.grupotres.appcongreso.databinding.ItemLectureBinding
 import org.grupotres.appcongreso.ui.helpers.INavigator
 import org.grupotres.appcongreso.util.setOnClickListener
 
 class LectureAdapter(private val viewModel: LectureViewModel, private val navigator: INavigator) :
 	ListAdapter<LectureSpeakers, LectureAdapter.LectureViewHolder>(LectureCallback()) {
 
-	inner class LectureViewHolder(private val binding: ItemLectureDetailBinding) :
+	private val colors = mapOf(
+		"Construcción" to "#0000FF",
+		"Ingeniería" to "#00FF00",
+		"Minería" to "#FF0000",
+		"Big Data" to "#800080",
+		"IoT" to "#FF00FF",
+		"Seguridad Informática" to "#FF4500",
+		"Estructuras" to "#FFFF00",
+		"Ecotoxicología" to "#008080",
+		"Informática" to "#00FFFF",
+		"Geología" to "#8B0000",
+	)
+
+	inner class LectureViewHolder(private val binding: ItemLectureBinding) :
 		RecyclerView.ViewHolder(binding.root) {
 
 		fun bind(lectureSpeaker: LectureSpeakers) {
 			binding.lectureTitle.text = lectureSpeaker.lecture.title
+			binding.topic.text = lectureSpeaker.lecture.topic
+			val color = Color.parseColor(colors[lectureSpeaker.lecture.topic])
+			binding.topicColor.setBackgroundColor(color)
 		}
 	}
 
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LectureViewHolder {
-		val binding = ItemLectureDetailBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+		val binding = ItemLectureBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 		val holder = LectureViewHolder(binding)
 		holder.setOnClickListener { position, _ -> goToLectureDetail(position) }
 		return holder
