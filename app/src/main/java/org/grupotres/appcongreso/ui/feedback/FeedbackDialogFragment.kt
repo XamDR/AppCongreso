d76bd0e33d3f86b46edb0e8c88f59ad5868708b0
SamBeCo <samuel_ibc@hotmail.com>
2021-05-27 15:52:08 -0500

    Feedback 100% terminado V1

--- app/src/main/java/org/grupotres/appcongreso/ui/feedback/FeedbackDialogFragment.kt
+++ app/src/main/java/org/grupotres/appcongreso/ui/feedback/FeedbackDialogFragment.kt
@@ -4,23 +4,52 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import org.grupotres.appcongreso.databinding.FragmentFeedbackDialogBinding
+import org.grupotres.appcongreso.ui.lectures.LectureListFragmentArgs
 
-class FeedbackDialogFragment : BottomSheetDialogFragment() {
+class FeedbackDialogFragment(idLecture: String) : BottomSheetDialogFragment() {
 
 	private var binding: FragmentFeedbackDialogBinding? = null
+	private var idU = idLecture
+	private val db = FirebaseFirestore.getInstance()
+	lateinit var auth: FirebaseAuth
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
 		binding = FragmentFeedbackDialogBinding.inflate(inflater, container, false)
+		binding?.btnSendMessage?.setOnClickListener { sendFeedback() }
 		return binding?.root
 	}
 
 	override fun onDestroyView() {
 		super.onDestroyView()
 		binding = null
+	}
+
+	private fun sendFeedback(){
+		auth = FirebaseAuth.getInstance()
+
+		val userEmail = auth.currentUser?.email.toString()
+		val name = auth.currentUser?.displayName
+		val primaryKey = "$userEmail$idU"
+		val rating = binding?.lectureRating?.rating
+
+
+		db.collection("comentarios").document(primaryKey).set(
+			hashMapOf(
+				"primaryKey" to primaryKey,
+				"idLecture" to idU,
+				"nameUser" to name,
+				"rating" to rating,
+				"coment" to binding?.comment?.text.toString()
+			)
+		)
+		onDestroyView()
 	}
 }
