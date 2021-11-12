71b6e0c7bcfc1b088a2e1d8508d556f8700e59e7
XamDR <maxdr.mat@gmail.com>
2021-11-12 11:44:25 -0500

    Se añadió Splash Activity.

---
+++ app/src/main/java/org/grupotres/appcongreso/SplashActivity.kt
@@ -0,0 +1,33 @@
+package org.grupotres.appcongreso
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+@Suppress("deprecation")
+class SplashActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+		window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+			WindowManager.LayoutParams.FLAG_FULLSCREEN
+		)
+		setContentView(R.layout.activity_splash)
+		Handler(Looper.getMainLooper()).postDelayed({
+			val intent = Intent(this@SplashActivity, MainActivity::class.java)
+			startActivity(intent)
+			finish()
+		}, DURACION_SPLASH)
+	}
+
+	companion object {
+		private const val DURACION_SPLASH = 500L
+	}
+}
