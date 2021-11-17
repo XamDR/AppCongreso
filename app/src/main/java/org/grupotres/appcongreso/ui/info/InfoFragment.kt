8b48c6e746b1d9cda276ad363b901e58b89ead47
XamDR <maxdr.mat@gmail.com>
2021-11-17 11:04:42 -0500

    Se actualizó la información de los pagos.

--- app/src/main/java/org/grupotres/appcongreso/ui/info/InfoFragment.kt
+++ app/src/main/java/org/grupotres/appcongreso/ui/info/InfoFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
+import edu.icontinental.congresoi40.R
 import edu.icontinental.congresoi40.databinding.InfoFragmentBinding
 
 class InfoFragment : Fragment() {
@@ -17,5 +19,13 @@ class InfoFragment : Fragment() {
 	): View? {
 		binding = InfoFragmentBinding.inflate(inflater, container, false)
 		return binding?.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		val textOption1 = HtmlCompat.fromHtml(getText(R.string.info_opc1).toString(), HtmlCompat.FROM_HTML_MODE_COMPACT)
+		val textOption2 = HtmlCompat.fromHtml(getText(R.string.info_opc2).toString(), HtmlCompat.FROM_HTML_MODE_COMPACT)
+		binding?.infoOpc1?.text = textOption1
+		binding?.infoOpc2?.text = textOption2
 	}
 }
